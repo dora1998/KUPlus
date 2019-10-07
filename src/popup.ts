8dd03b9b1220408e2ef6a90e38a52597b59b75ad
dora <31735614+dora1998@users.noreply.github.com>
2019-10-07 14:18:53 +0900

    Do not create new tab when location is 'chrome://newtab/'

--- src/popup.ts
+++ src/popup.ts
@@ -2,7 +2,16 @@ window.onload = () => {
   document.querySelectorAll(".container button").forEach((b) => {
     b.addEventListener('click', (e) => {
       const url = b.getAttribute("href") || 'http://www.kyoto-u.ac.jp';
-      chrome.tabs.create({url});
+
+      // 新規タブページにいたら、新しくタブを開かない
+      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        if (tabs.length > 0 && tabs[0].url === 'chrome://newtab/') {
+          chrome.tabs.update({url});
+          window.close();
+        } else {
+          chrome.tabs.create({url});
+        }
+      });
       return false;
     });
   });
